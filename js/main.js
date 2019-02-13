9b2c36f1c8c8c4945712507efc9e15634230539c
MikuHatsune39 <32179075+MikuHatsune39@users.noreply.github.com>
2019-02-13 12:36:00 -0800

    Added Jiji image page

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var photos = ["./img/sitting.png", "./img/laying.jpg", "./img/stretching.jpg"];
+var position = 0;
+
+function nextPhoto()
+{
+    if(position < 2)
+    {
+        document.getElementById("photo").src = photos[++position];
+    }
+    else if(position == 2)
+    {
+        document.getElementById("photo").src = photos[0];
+        position = 0;
+    }
+}
+
+function prevPhoto()
+{
+    if(position > 0)
+    {
+        document.getElementById("photo").src = photos[--position];
+    }
+    else if(position == 0)
+    {
+        document.getElementById("photo").src = photos[2];
+        position = 2;
+    }
+}
