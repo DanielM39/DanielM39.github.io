6325a4bac6ccd6990ab52d4577fa6badd3bf4e9a
MikuHatsune39 <32179075+MikuHatsune39@users.noreply.github.com>
2019-03-21 21:56:54 -0700

    Added img set 2

--- img/sets/MakeSmallerImgs.bat
+++ img/sets/MakeSmallerImgs.bat
@@ -1,25 +1,25 @@
 echo converting exposure 0
-ffmpeg -i 0.png -vf scale=3000:2000 0(mid).png
-ffmpeg -i 0.png -vf scale=600:400 0(small).png
-ffmpeg -i 0.jpg -vf scale=3000:2000 0(mid).jpg
-ffmpeg -i 0.jpg -vf scale=600:400 0(small).jpg
+rem ffmpeg -i 0.png -vf scale=3000:2000 0(mid).png
+rem ffmpeg -i 0.png -vf scale=600:400 0(small).png
+rem ffmpeg -i 0(mid).jpg -vf scale=3000:2000 0(mid).jpg
+ffmpeg -i 0(mid).jpg -vf scale=600:400 0(small).jpg
 echo converting exposure 1
-ffmpeg -i 1.png -vf scale=3000:2000 1(mid).png
-ffmpeg -i 1.png -vf scale=600:400 1(small).png
-ffmpeg -i 1.jpg -vf scale=3000:2000 1(mid).jpg
-ffmpeg -i 1.jpg -vf scale=600:400 1(small).jpg
+rem ffmpeg -i 1.png -vf scale=3000:2000 1(mid).png
+rem ffmpeg -i 1.png -vf scale=600:400 1(small).png
+rem ffmpeg -i 1(mid).jpg -vf scale=3000:2000 1(mid).jpg
+ffmpeg -i 1(mid).jpg -vf scale=600:400 1(small).jpg
 echo converting exposure 2
-ffmpeg -i 2.png -vf scale=3000:2000 2(mid).png
-ffmpeg -i 2.png -vf scale=600:400 2(small).png
-ffmpeg -i 2.jpg -vf scale=3000:2000 2(mid).jpg
-ffmpeg -i 2.jpg -vf scale=600:400 2(small).jpg
+rem ffmpeg -i 2.png -vf scale=3000:2000 2(mid).png
+rem ffmpeg -i 2.png -vf scale=600:400 2(small).png
+rem ffmpeg -i 2(mid).jpg -vf scale=3000:2000 2(mid).jpg
+ffmpeg -i 2(mid).jpg -vf scale=600:400 2(small).jpg
 echo converting exposure -1
-ffmpeg -i ./-1.png -vf scale=3000:2000 ./-1(mid).png
-ffmpeg -i ./-1.png -vf scale=600:400 ./-1(small).png
-ffmpeg -i ./-1.jpg -vf scale=3000:2000 ./-1(mid).jpg
-ffmpeg -i ./-1.jpg -vf scale=600:400 ./-1(small).jpg
+rem ffmpeg -i ./-1.png -vf scale=3000:2000 ./-1(mid).png
+rem ffmpeg -i ./-1.png -vf scale=600:400 ./-1(small).png
+rem ffmpeg -i ./-1(mid).jpg -vf scale=3000:2000 ./-1(mid).jpg
+ffmpeg -i ./-1(mid).jpg -vf scale=600:400 ./-1(small).jpg
 echo converting exposure -2
-ffmpeg -i ./-2.png -vf scale=3000:2000 ./-2(mid).png
-ffmpeg -i ./-2.png -vf scale=600:400 ./-2(small).png
-ffmpeg -i ./-2.jpg -vf scale=3000:2000 ./-2(mid).jpg
-ffmpeg -i ./-2.jpg -vf scale=600:400 ./-2(small).jpg
+rem ffmpeg -i ./-2.png -vf scale=3000:2000 ./-2(mid).png
+rem ffmpeg -i ./-2.png -vf scale=600:400 ./-2(small).png
+rem ffmpeg -i ./-2(mid).jpg -vf scale=3000:2000 ./-2(mid).jpg
+ffmpeg -i ./-2(mid).jpg -vf scale=600:400 ./-2(small).jpg
--- js/raw-vs-jpeg.js
+++ js/raw-vs-jpeg.js
@@ -1,6 +1,20 @@
+/*
+$('.imgSet').click(function(){
+    $('.imgSet').removeClass('on').addClass('off');
+    $(this).addClass('on').removeClass('off');
+});
+$('.exp').click(function(){
+    $('.exp').removeClass('on').addClass('off');
+    $(this).addClass('on').removeClass('off');
+});
+*/
 //var set1 = [new ImgPair("./img/sets/1/0.png", "./img/sets/1/0.jpg"), new ImgPair("./img/sets/1/1.png", "./img/sets/1/1.jpg"), new ImgPair("./img/sets/1/2.png", "./img/sets/1/2.jpg"), new ImgPair("./img/sets/1/-1.png", "./img/sets/1/-1.jpg"), new ImgPair("./img/sets/1/-2.png", "./img/sets/1/-2.jpg")];
 var set1 = [new ImgPair("./img/sets/1/0(mid).png", "./img/sets/1/0(mid).jpg"), new ImgPair("./img/sets/1/1(mid).png", "./img/sets/1/1(mid).jpg"), new ImgPair("./img/sets/1/2(mid).png", "./img/sets/1/2(mid).jpg"), new ImgPair("./img/sets/1/-1(mid).png", "./img/sets/1/-1(mid).jpg"), new ImgPair("./img/sets/1/-2(mid).png", "./img/sets/1/-2(mid).jpg")];
 var set1s = [new ImgPair("./img/sets/1/0(small).png", "./img/sets/1/0(small).jpg"), new ImgPair("./img/sets/1/1(small).png", "./img/sets/1/1(small).jpg"), new ImgPair("./img/sets/1/2(small).png", "./img/sets/1/2(small).jpg"), new ImgPair("./img/sets/1/-1(small).png", "./img/sets/1/-1(small).jpg"), new ImgPair("./img/sets/1/-2(small).png", "./img/sets/1/-2(small).jpg")];
+
+var set2 = [new ImgPair("./img/sets/2/0(mid).png", "./img/sets/2/0(mid).jpg"), new ImgPair("./img/sets/2/1(mid).png", "./img/sets/2/1(mid).jpg"), new ImgPair("./img/sets/2/2(mid).png", "./img/sets/2/2(mid).jpg"), new ImgPair("./img/sets/2/-1(mid).png", "./img/sets/2/-1(mid).jpg"), new ImgPair("./img/sets/2/-2(mid).png", "./img/sets/2/-2(mid).jpg")];
+var set2s = [new ImgPair("./img/sets/2/0(small).png", "./img/sets/2/0(small).jpg"), new ImgPair("./img/sets/2/1(small).png", "./img/sets/2/1(small).jpg"), new ImgPair("./img/sets/2/2(small).png", "./img/sets/2/2(small).jpg"), new ImgPair("./img/sets/2/-1(small).png", "./img/sets/2/-1(small).jpg"), new ImgPair("./img/sets/2/-2(small).png", "./img/sets/2/-2(small).jpg")];
+
 var set = 1;
 
 function ImgPair(raw, jpeg)
@@ -20,7 +34,11 @@ function displayImgs(exposure)
             //console.log(set1s[exposure].jpeg);
             switchToLargerImgs(set1[exposure].raw, set1[exposure].jpeg);
             break;
-        
+        case 2:
+            document.getElementById("raw").src = set2s[exposure].raw;
+            document.getElementById("jpeg").src = set2s[exposure].jpeg;
+            switchToLargerImgs(set2[exposure].raw, set2[exposure].jpeg);
+            break;
         default:
             break;
     }
