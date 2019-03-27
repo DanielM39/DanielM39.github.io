5fba4868cb6082f8017c4b4964ea28fba06b3d87
DanielM39 <46549393+DanielM39@users.noreply.github.com>
2019-03-26 23:17:12 -0700

    Updated controlls

--- js/raw-vs-jpeg.js
+++ js/raw-vs-jpeg.js
@@ -15,7 +15,17 @@ var set1s = [new ImgPair("./img/sets/1/0(small).png", "./img/sets/1/0(small).jpg
 var set2 = [new ImgPair("./img/sets/2/0(mid).png", "./img/sets/2/0(mid).jpg"), new ImgPair("./img/sets/2/1(mid).png", "./img/sets/2/1(mid).jpg"), new ImgPair("./img/sets/2/2(mid).png", "./img/sets/2/2(mid).jpg"), new ImgPair("./img/sets/2/-1(mid).png", "./img/sets/2/-1(mid).jpg"), new ImgPair("./img/sets/2/-2(mid).png", "./img/sets/2/-2(mid).jpg")];
 var set2s = [new ImgPair("./img/sets/2/0(small).png", "./img/sets/2/0(small).jpg"), new ImgPair("./img/sets/2/1(small).png", "./img/sets/2/1(small).jpg"), new ImgPair("./img/sets/2/2(small).png", "./img/sets/2/2(small).jpg"), new ImgPair("./img/sets/2/-1(small).png", "./img/sets/2/-1(small).jpg"), new ImgPair("./img/sets/2/-2(small).png", "./img/sets/2/-2(small).jpg")];
 
-var set3 = 
+var set3 = [new ImgPair("./img/sets/3/0(mid).png", "./img/sets/3/0(mid).jpg"), new ImgPair("./img/sets/3/1(mid).png", "./img/sets/3/1(mid).jpg"), new ImgPair("./img/sets/3/2(mid).png", "./img/sets/3/2(mid).jpg"), new ImgPair("./img/sets/3/-1(mid).png", "./img/sets/3/-1(mid).jpg"), new ImgPair("./img/sets/3/-2(mid).png", "./img/sets/3/-2(mid).jpg")];
+var set3s = [new ImgPair("./img/sets/3/0(small).png", "./img/sets/3/0(small).jpg"), new ImgPair("./img/sets/3/1(small).png", "./img/sets/3/1(small).jpg"), new ImgPair("./img/sets/3/2(small).png", "./img/sets/3/2(small).jpg"), new ImgPair("./img/sets/3/-1(small).png", "./img/sets/3/-1(small).jpg"), new ImgPair("./img/sets/3/-2(small).png", "./img/sets/3/-2(small).jpg")];
+
+var set4 = [new ImgPair("./img/sets/4/0(mid).png", "./img/sets/4/0(mid).jpg"), new ImgPair("./img/sets/4/1(mid).png", "./img/sets/4/1(mid).jpg"), new ImgPair("./img/sets/4/2(mid).png", "./img/sets/4/2(mid).jpg"), new ImgPair("./img/sets/4/-1(mid).png", "./img/sets/4/-1(mid).jpg"), new ImgPair("./img/sets/4/-2(mid).png", "./img/sets/4/-2(mid).jpg")];
+var set4s = [new ImgPair("./img/sets/4/0(small).png", "./img/sets/4/0(small).jpg"), new ImgPair("./img/sets/4/1(small).png", "./img/sets/4/1(small).jpg"), new ImgPair("./img/sets/4/2(small).png", "./img/sets/4/2(small).jpg"), new ImgPair("./img/sets/4/-1(small).png", "./img/sets/4/-1(small).jpg"), new ImgPair("./img/sets/4/-2(small).png", "./img/sets/4/-2(small).jpg")];
+
+var set5 = [new ImgPair("./img/sets/5/0(mid).png", "./img/sets/5/0(mid).jpg"), new ImgPair("./img/sets/5/1(mid).png", "./img/sets/5/1(mid).jpg"), new ImgPair("./img/sets/5/2(mid).png", "./img/sets/5/2(mid).jpg"), new ImgPair("./img/sets/5/-1(mid).png", "./img/sets/5/-1(mid).jpg"), new ImgPair("./img/sets/5/-2(mid).png", "./img/sets/5/-2(mid).jpg")];
+var set5s = [new ImgPair("./img/sets/5/0(small).png", "./img/sets/5/0(small).jpg"), new ImgPair("./img/sets/5/1(small).png", "./img/sets/5/1(small).jpg"), new ImgPair("./img/sets/5/2(small).png", "./img/sets/5/2(small).jpg"), new ImgPair("./img/sets/5/-1(small).png", "./img/sets/5/-1(small).jpg"), new ImgPair("./img/sets/5/-2(small).png", "./img/sets/5/-2(small).jpg")];
+
+var set6 = [new ImgPair("./img/sets/6/0(mid).png", "./img/sets/6/0(mid).jpg"), new ImgPair("./img/sets/6/1(mid).png", "./img/sets/6/1(mid).jpg"), new ImgPair("./img/sets/6/2(mid).png", "./img/sets/6/2(mid).jpg"), new ImgPair("./img/sets/6/-1(mid).png", "./img/sets/6/-1(mid).jpg"), new ImgPair("./img/sets/6/-2(mid).png", "./img/sets/6/-2(mid).jpg")];
+var set6s = [new ImgPair("./img/sets/6/0(small).png", "./img/sets/6/0(small).jpg"), new ImgPair("./img/sets/6/1(small).png", "./img/sets/6/1(small).jpg"), new ImgPair("./img/sets/6/2(small).png", "./img/sets/6/2(small).jpg"), new ImgPair("./img/sets/6/-1(small).png", "./img/sets/6/-1(small).jpg"), new ImgPair("./img/sets/6/-2(small).png", "./img/sets/6/-2(small).jpg")];
 
 var set = 1;
 var ex = 0;
@@ -62,6 +72,26 @@ function displayImgs(exposure)
             document.getElementById("raw").src = set2s[exposure].raw;
             document.getElementById("jpeg").src = set2s[exposure].jpeg;
             switchToLargerImgs(set2[exposure].raw, set2[exposure].jpeg);
+            break;
+        case 3:
+            document.getElementById("raw").src = set3s[exposure].raw;
+            document.getElementById("jpeg").src = set3s[exposure].jpeg;
+            switchToLargerImgs(set3[exposure].raw, set3[exposure].jpeg);
+            break;
+        case 4:
+            document.getElementById("raw").src = set4s[exposure].raw;
+            document.getElementById("jpeg").src = set4s[exposure].jpeg;
+            switchToLargerImgs(set4[exposure].raw, set4[exposure].jpeg);
+            break;
+        case 5:
+            document.getElementById("raw").src = set5s[exposure].raw;
+            document.getElementById("jpeg").src = set5s[exposure].jpeg;
+            switchToLargerImgs(set5[exposure].raw, set5[exposure].jpeg);
+            break;
+        case 6:
+            document.getElementById("raw").src = set6s[exposure].raw;
+            document.getElementById("jpeg").src = set6s[exposure].jpeg;
+            switchToLargerImgs(set6[exposure].raw, set6[exposure].jpeg);
             break;
         default:
             break;
